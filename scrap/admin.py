f2be60609e397477096e527362083cfb48f0bc66
Maksim Masterenko <mmasterenko@gmail.com>
2016-12-13 21:38:32 +0300

    :rocket: models Ads, Categories

--- scrap/admin.py
+++ scrap/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ads, Categories
 
-# Register your models here.
+
+@admin.register(Ads)
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ['header', 'price', 'city', 'position']
+    list_filter = ['category__name']
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
--- scrap/models.py
+++ scrap/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField('категория', max_length=256)
+    url = models.URLField('URL', default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Ads(models.Model):
+    category = models.ForeignKey(Categories, verbose_name='категория', blank=True, null=True)
+    header = models.CharField('заголовок', max_length=256)
+    city = models.CharField('город', max_length=64)
+    price = models.CharField('цена', max_length=64)
+    position = models.IntegerField('позиция объявления')
+
+    def __str__(self):
+        return self.header
