1cf80fd09ea68486f87e0e759a95d330a6a0a56e
Maksim Masterenko <mmasterenko@gmail.com>
2016-12-13 21:48:22 +0300

    :wrench: Categories.url null, blank

--- scrap/models.py
+++ scrap/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField('категория', max_length=256)
-    url = models.URLField('URL', default='')
+    url = models.URLField('URL', default='', null=True, blank=True)
 
     def __str__(self):
         return self.name
